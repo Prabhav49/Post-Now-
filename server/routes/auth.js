36b70a84ae648f1cbd43b0db0902f7dd71851e99
Prabhav <iamprabhav23@gmail.com>
2023-04-29 16:30:17 +0530

    Resolved PostWidget Error Successfully - Only User Picture Path is Remaining

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,7 @@
 import express from "express";
-import {login} from "../controllers/auth.js";
+import { login } from "../controllers/auth.js";
 
-const router =  express.Router();
+const router = express.Router();
 
 router.post("/login", login);
 
